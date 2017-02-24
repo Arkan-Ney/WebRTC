bb0790e5b33eef024bc5e52db974474d4b3f7fd0
david toron <david@toron.popschool>
2017-02-24 12:21:28 +0100

    modif app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+function bindEvents(p) {
+
+		p.on('signal', function (data) {
+			
+		})
+
+}
+
 document.querySelector('#start').addEventListener('click', function (e) {
     navigator.getUserMedia({
         video: true,
@@ -6,9 +14,12 @@ document.querySelector('#start').addEventListener('click', function (e) {
 		let p = new simplePeer ({
 			initiator: true,
 			stream: stream,
-			config
-		})
+		// Config pour STUN & TURN	
+		// config
+	})
+		bindEvents(p)
         let emitterVideo = document.querySelector('#emitter-video')
+		emitterVideo.volume = 0
         emitterVideo.src = window.URL.createObjectURL(stream)
         emitterVideo.play()
     }, function () {})
